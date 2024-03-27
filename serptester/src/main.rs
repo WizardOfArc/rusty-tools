544a669d994f02b4be73dc15db5a58dffbb145e9
Azi <azicrawford@gmail.com>
2024-03-27 09:20:02 -0700

    fix serptester url formatting

--- serptester/src/main.rs
+++ serptester/src/main.rs
@@ -3,6 +3,22 @@ use webbrowser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+
+enum Environment {
+    Local,
+    Stage,
+    Prod,
+}
+
+enum PageType {
+    Hosted,
+    Embedded,
+    Forwarding,
+    Api,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct Args {
     /// domain to check
     domain: String,
@@ -17,31 +33,65 @@ struct Args {
 }
 
 const SERP_PATH: &str = "/serp?q=hotels&debug=show";
-const EMBEDDED_DEMO_PATH: &str = ".s1search.co/embedded-demo?q=hotels&debug=show#S1&debug=1";
+const EMBEDDED_DEMO_PATH: &str = "/embedded-demo?q=hotels&debug=show#S1&debug=1";
 
+fn build_path(page_type: PageType) -> String {
+    match page_type {
+        PageType::Hosted => SERP_PATH.to_string(),
+        PageType::Embedded => EMBEDDED_DEMO_PATH.to_string(),
+        _ => SERP_PATH.to_string(),
+    }
+}
+
+fn build_local_domain(domain: &str, page_type: PageType) -> String {
+    format!("http://local.{}:8080{}", domain, build_path(page_type))
+}
+
+fn build_prod_domain(domain: &str, page_type: PageType) -> String {
+    match page_type {
+        PageType::Hosted => format!("https://{}{}", domain, build_path(page_type)),
+        _ => format!("https://{}{}", domain.replace(".", "-"), build_path(page_type)),
+    }
+}
+
+fn build_stage_domain(domain: &str, unit: Option<String>, page_type: PageType) -> String {
+    let normailzed_domain = match page_type {
+        PageType::Hosted => format!("{}", domain),
+        _ => format!("{}", domain.replace(".", "-")),
+    };
+
+    match unit {
+        None => format!("http://{}.search-stage.system1.company{}", normailzed_domain, build_path(page_type)),
+        Some(unit_name) => format!("http://{}.search-stage-{}.system1.company{}", normailzed_domain, unit_name, build_path(page_type)),
+    }
+}
 
 fn build_url(settings: &Args) -> String {
-    let host_root: String = match settings.env.as_str() {
-        "l" | "local" => format!("local.{}", settings.domain),
-        _ => match settings.page_type.as_str() {
-            "h" | "hosted" => format!("{}", settings.domain),
-            _ => settings.domain.replace(".", "-"),
-        }
+    let env: Environment = match settings.env.as_str() {
+        "l" | "local" => Environment::Local,
+        "s" | "stage" => Environment::Stage,
+        _ => Environment::Prod,
     };
-    let host_suffix: String = match settings.env.as_str() {
-        "l" | "local" => format!("{}", ":8080"),
-        "s" | "stage" => match settings.unit.as_str() {
-            "X" => format!("{}",  ".search-stage.system1.company"),
-            _ => format!(".search-stage-{}.system1.company", settings.unit),
-        },
-        _ => String::from("")
-    };
-    let path_string: String = match settings.page_type.as_str() {
-        "e" | "embedded" => format!("{}", EMBEDDED_DEMO_PATH),
-        _ => format!("{}", SERP_PATH),
-    };
-    format!("http://{}{}{}", host_root, host_suffix, path_string)
 
+    let page_type: PageType = match settings.page_type.as_str() {
+        "h" | "hosted" => PageType::Hosted,
+        "e" | "embedded" => PageType::Embedded,
+        "f" | "forwarding" => PageType::Forwarding,
+        "a" | "api" => PageType::Api,
+        _ => PageType::Hosted,
+    };
+
+    let unit_option: Option<String> = match settings.unit.as_str() {
+        "X" => None,
+        name => Some(name.to_string()),
+    };  
+    
+    let domain = &settings.domain;
+    match env {
+        Environment::Local => build_local_domain(domain, page_type),
+        Environment::Stage => build_stage_domain(domain, unit_option, page_type),
+        Environment::Prod => build_prod_domain(domain, page_type),
+    }
 }
 
 fn show_settings(settings: &Args) {
