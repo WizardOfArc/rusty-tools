4396c6486a1736610ab6d1f0fc945f3326f92038
Azi <azicrawford@gmail.com>
2024-08-16 13:09:21 -0700

    do a double ended BFS to make it go faster

--- anagram_transform/src/main.rs
+++ anagram_transform/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 
 use clap::Parser;
@@ -59,23 +60,66 @@ fn path_from_parent_mapping(mapping: &HashMap<String, Option<String>>, child: &S
     path.into_iter().rev().collect()
 }
 
+fn path_from_both_parent_mapping(forward_mapping: &HashMap<String, Option<String>>, backward_mapping: &HashMap<String, Option<String>>, child: &String) -> Vec<String> {
+    let mut forward_path: Vec<String> = path_from_parent_mapping(forward_mapping, child);
+    let mut reverse_path: Vec<String> = path_from_parent_mapping(backward_mapping, child);
+    reverse_path.reverse();
+    for word in reverse_path[1..].iter() {
+        forward_path.push(word.to_string());
+    }
+    forward_path
+}
+
 
 fn find_shortest_path_bewteen_words(word_a: &str, word_b: &str) -> Vec<String> {
     let mut parent_mapping: HashMap<String, Option<String>> = HashMap::new();
+    let mut reverse_parent_mapping: HashMap<String, Option<String>> = HashMap::new();
+    let mut seen_words: HashSet<String> = HashSet::new();
+    let mut reverse_seen_words: HashSet<String> = HashSet::new();
     let swappables = get_swappable_indexes(word_a, word_b);
     let mut word_queue: VecDeque<String> = VecDeque::new();
+    let mut reverse_word_queue: VecDeque<String> = VecDeque::new();
     parent_mapping.insert(word_a.to_string(), None);
+    reverse_parent_mapping.insert(word_b.to_string(), None);
+    seen_words.insert(word_a.to_string());
+    reverse_seen_words.insert(word_b.to_string());
     word_queue.push_back(word_a.to_string());
-    while word_queue.len() > 0 {
+    reverse_word_queue.push_back(word_b.to_string());
+    while word_queue.len() > 0 && reverse_word_queue.len() > 0 {
         let current_word = word_queue.pop_front().unwrap();
+        if reverse_parent_mapping.contains_key(&current_word.clone()) {
+            return path_from_both_parent_mapping(&parent_mapping, &reverse_parent_mapping, &current_word.clone());
+        }
+        let current_reverse_word = reverse_word_queue.pop_front().unwrap();
+        if parent_mapping.contains_key(&current_reverse_word.clone()) {
+            return path_from_both_parent_mapping(&reverse_parent_mapping, &parent_mapping, &current_reverse_word.clone());
+        }
         for meta_idx_2 in 1..swappables.len() {
             for meta_idx_1 in 0..meta_idx_2 {
               let idx1 = swappables[meta_idx_1];
               let idx2 = swappables[meta_idx_2];
               let new_word = new_string_from_swap(&current_word.clone(), idx1, idx2);
+              let reverse_new_word = new_string_from_swap(&current_reverse_word.clone(), idx1, idx2);
+              if seen_words.contains(&new_word.clone()) {
+                  continue;
+              }
+              if reverse_seen_words.contains(&reverse_new_word.clone()) {
+                  continue;
+              }
+              reverse_seen_words.insert(reverse_new_word.clone());
+              seen_words.insert(new_word.clone());
+              if !reverse_parent_mapping.contains_key(&reverse_new_word.clone()){
+                  reverse_parent_mapping.insert(reverse_new_word.clone(), Some(current_reverse_word.clone()));
+                  reverse_word_queue.push_back(reverse_new_word.clone()); 
+              }
               if !parent_mapping.contains_key(&new_word.clone()){
                   parent_mapping.insert(new_word.clone(), Some(current_word.clone()));
                   word_queue.push_back(new_word.clone());
+              }
+              if reverse_new_word == word_a.to_string() {
+                  let mut path = path_from_parent_mapping(&reverse_parent_mapping, &reverse_new_word);
+                  path.reverse();
+                  return path;
               }
               if new_word == word_b.to_string() {
                   return path_from_parent_mapping(&parent_mapping, &new_word)
