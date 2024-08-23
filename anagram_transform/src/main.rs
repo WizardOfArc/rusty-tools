cdea37420761e368c46b851ec0b5085a28955a63
Azi <azicrawford@gmail.com>
2024-08-23 12:00:50 -0700

    pass in a swappable indexes vector - maye want to replace thsi with a priority queue

--- anagram_transform/src/main.rs
+++ anagram_transform/src/main.rs
@@ -16,6 +16,12 @@ enum Direction {
     Backward,
 }
 
+#[derive(Debug, Clone)]
+struct WordWithSwappables {
+    word: String,
+    swappables: Option<Vec<usize>>,
+}
+
 
 // make a function that gets a new word based on swapping two chars at given indexes
 fn new_string_from_swap(word: &str, idx1: usize, idx2: usize) -> String {
@@ -32,14 +38,25 @@ fn new_string_from_swap(word: &str, idx1: usize, idx2: usize) -> String {
     char_vec.iter().collect::<String>()
 }
 
-fn get_swappable_indexes(word_a: &str, word_b: &str) -> Vec<usize> {
+fn get_swappable_indexes(source_word: &str, destination_word: &str, previous_swappables: Option<Vec<usize>>) -> Vec<usize> {
     let mut swappables: Vec<usize> = Vec::new();
-    let length = word_a.len();
-    let word_a_chars: Vec<char> = word_a.chars().collect();
-    let word_b_chars: Vec<char> = word_b.chars().collect();
-    for index in 0..length {
-        if word_a_chars[index] != word_b_chars[index] {
-            swappables.push(index);
+    let length = source_word.len();
+    let src_word_chars: Vec<char> = source_word.chars().collect();
+    let target_word_chars: Vec<char> = destination_word.chars().collect();
+    match previous_swappables {
+        Some(prev_indexes) => {
+            for index in prev_indexes {
+                if src_word_chars[index] != target_word_chars[index] {
+                    swappables.push(index);
+                }
+            }
+        }
+        None => {
+            for index in 0..length {
+                if src_word_chars[index] != target_word_chars[index] {
+                    swappables.push(index);
+                }
+            }
         }
     }
     swappables
@@ -77,58 +94,96 @@ fn path_from_both_parent_mapping(forward_mapping: &HashMap<String, Option<String
     forward_path
 }
 
-
+// I want to narrow down the available swaps as we progress through the BFS and get closer
+// an alternative would be to use a priority queue where closer words are given higher priority
 fn find_shortest_path_bewteen_words(word_a: &str, word_b: &str) -> Vec<String> {
     let mut parent_mapping: HashMap<String, Option<String>> = HashMap::new();
     let mut reverse_parent_mapping: HashMap<String, Option<String>> = HashMap::new();
     let mut seen_words: HashSet<String> = HashSet::new();
     let mut reverse_seen_words: HashSet<String> = HashSet::new();
-    let swappables = get_swappable_indexes(word_a, word_b);
-    let mut word_queue: VecDeque<String> = VecDeque::new();
-    let mut reverse_word_queue: VecDeque<String> = VecDeque::new();
+    let swappables = get_swappable_indexes(word_a, word_b, None);
+    let mut word_queue: VecDeque<WordWithSwappables> = VecDeque::new();
+    let mut reverse_word_queue: VecDeque<WordWithSwappables> = VecDeque::new();
     parent_mapping.insert(word_a.to_string(), None);
     reverse_parent_mapping.insert(word_b.to_string(), None);
     seen_words.insert(word_a.to_string());
     reverse_seen_words.insert(word_b.to_string());
-    word_queue.push_back(word_a.to_string());
-    reverse_word_queue.push_back(word_b.to_string());
+    word_queue.push_back(
+        WordWithSwappables {
+            word: word_a.to_string(),
+            swappables: Some(swappables.clone())
+        }
+    );
+    reverse_word_queue.push_back(
+        WordWithSwappables {
+            word: word_b.to_string(),
+            swappables: Some(swappables)
+        }
+    );
     while word_queue.len() > 0 && reverse_word_queue.len() > 0 {
-        let current_word = word_queue.pop_front().unwrap();
+        let current_word_with_swappables= word_queue.pop_front().unwrap();
+        let current_word = current_word_with_swappables.word;
+        let current_swappables = current_word_with_swappables.swappables.unwrap();
         if reverse_parent_mapping.contains_key(&current_word.clone()) {
             return path_from_both_parent_mapping(&parent_mapping, &reverse_parent_mapping, &current_word.clone());
         }
-        let current_reverse_word = reverse_word_queue.pop_front().unwrap();
+        let current_reverse_word_with_swappables = reverse_word_queue.pop_front().unwrap();
+        let current_reverse_word = current_reverse_word_with_swappables.word;
+        let reverse_swappables = current_reverse_word_with_swappables.swappables.unwrap();
         if parent_mapping.contains_key(&current_reverse_word.clone()) {
             return path_from_both_parent_mapping(&reverse_parent_mapping, &parent_mapping, &current_reverse_word.clone());
         }
-        for meta_idx_2 in 1..swappables.len() {
+        // forward
+        for meta_idx_2 in 1..current_swappables.len() {
             for meta_idx_1 in 0..meta_idx_2 {
-              let idx1 = swappables[meta_idx_1];
-              let idx2 = swappables[meta_idx_2];
+              let idx1 = current_swappables[meta_idx_1];
+              let idx2 = current_swappables[meta_idx_2];
               let new_word = new_string_from_swap(&current_word.clone(), idx1, idx2);
-              let reverse_new_word = new_string_from_swap(&current_reverse_word.clone(), idx1, idx2);
               if seen_words.contains(&new_word.clone()) {
                   continue;
               }
-              if reverse_seen_words.contains(&reverse_new_word.clone()) {
-                  continue;
-              }
-              reverse_seen_words.insert(reverse_new_word.clone());
               seen_words.insert(new_word.clone());
-              if !reverse_parent_mapping.contains_key(&reverse_new_word.clone()){
-                  reverse_parent_mapping.insert(reverse_new_word.clone(), Some(current_reverse_word.clone()));
-                  reverse_word_queue.push_back(reverse_new_word.clone()); 
-              }
               if !parent_mapping.contains_key(&new_word.clone()){
                   parent_mapping.insert(new_word.clone(), Some(current_word.clone()));
-                  word_queue.push_back(new_word.clone());
-              }
-              if reverse_new_word == word_a.to_string() {
-                  return path_from_parent_mapping(&reverse_parent_mapping, &reverse_new_word, Direction::Forward);
+                  word_queue.push_back(
+                    WordWithSwappables {
+                        word: new_word.clone(),
+                        swappables: Some(
+                            get_swappable_indexes(&new_word, word_b, Some(current_swappables.clone()))
+                        )
+                    }
+                );
               }
               if new_word == word_b.to_string() {
                   return path_from_parent_mapping(&parent_mapping, &new_word, Direction::Backward)
               }
+            }
+        }
+        // backward
+        for reverse_meta_idx_2 in 1..reverse_swappables.len() {
+            for reverse_meta_idx_1 in 0..reverse_meta_idx_2 {
+              let idx1 = reverse_swappables[reverse_meta_idx_1];
+              let idx2 = reverse_swappables[reverse_meta_idx_2];
+              let reverse_new_word = new_string_from_swap(&current_reverse_word.clone(), idx1, idx2);
+              if reverse_seen_words.contains(&reverse_new_word.clone()) {
+                  continue;
+              }
+              reverse_seen_words.insert(reverse_new_word.clone());
+              if !reverse_parent_mapping.contains_key(&reverse_new_word.clone()){
+                  reverse_parent_mapping.insert(reverse_new_word.clone(), Some(current_reverse_word.clone()));
+                  reverse_word_queue.push_back(
+                    WordWithSwappables {
+                        word: reverse_new_word.clone(),
+                        swappables: Some(
+                            get_swappable_indexes(&reverse_new_word, word_a, Some(reverse_swappables.clone()))
+                        )
+                    }
+                ); 
+              }
+              if reverse_new_word == word_a.to_string() {
+                  return path_from_parent_mapping(&reverse_parent_mapping, &reverse_new_word, Direction::Forward);
+              }
+
             }
         }
     }
